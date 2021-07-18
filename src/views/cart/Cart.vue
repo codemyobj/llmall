b03e42b99964013fea27d7d1bd8d59b8d033a840
codemyobj <459324704@qq.com>
2021-07-18 23:16:09 +0800

    购物车导航栏完成

--- src/views/cart/Cart.vue
+++ src/views/cart/Cart.vue
@@ -1,14 +1,34 @@
 <template>
-  <div class>购物车</div>
+  <div class>
+    <nav-bar class="nav-bar">
+      <div slot="center">
+        购物车 (<span>{{ cartLength }}</span
+        >)
+      </div>
+    </nav-bar>
+    <cart-list />
+  </div>
 </template>
 
 <script>
+import NavBar from "@/components/common/navbar/NavBar.vue";
+import CartList from "./children/CartList.vue";
+import { mapGetters } from "vuex";
+
 export default {
   name: "Cart",
-  components: {},
+  components: {
+    NavBar,
+    CartList,
+  },
   props: {},
   data() {
     return {};
+  },
+  computed: {
+    ...mapGetters({
+      cartLength: "cartListLength",
+    }),
   },
 };
 </script>
---
+++ src/views/cart/children/CartList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="cart-list">
+    <scroll class="scoll-heiht"> </scroll>
+  </div>
+</template>
+
+<script>
+import Scroll from "@/components/common/scroll/Scroll.vue";
+import { mapGetters } from "vuex";
+
+export default {
+  name: "CartList",
+  components: {
+    Scroll,
+  },
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapGetters(["cartList", "cartListLength"]),
+  },
+};
+</script>
+<style scoped>
+</style>
